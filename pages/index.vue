89c5aff435a87ef41c32bf84ed8ccdb1f39b3fe9
SatyamChauhan <satyamc671@gmail.com>
2025-02-28 21:28:10 +0530

    `Added Back button on details page`

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="container mx-auto p-4">
-    <h1 class="text-2xl font-bold mb-4 bg-blue-100 text-center p-2 rounded">Posts</h1>
+    <h1 class="text-2xl font-bold mb-4 bg-blue-200 text-center p-2 rounded">Posts</h1>
     <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
       <PostCard v-for="post in posts" :key="post.id" :post="post" />
     </div>
--- pages/posts/[id].vue
+++ pages/posts/[id].vue
@@ -1,11 +1,17 @@
 <template>
   <div class="container mx-auto p-4">
-    <h1 class="text-2xl font-bold mb-6 bg-blue-100 text-center p-2 rounded">
+    <h1 class="text-2xl font-bold mb-6 bg-blue-300 text-center p-2 rounded">
       Post Details
     </h1>
     <div v-if="post" class="bg-white p-4 rounded shadow-md">
       <h2 class="text-xl font-semibold">{{ post.title }}</h2>
       <p class="text-gray-600">{{ post.body }}</p>
+      <button
+        class="bg-yellow-500 px-5 py-3 text-white rounded-md font-bold my-4 cursor-pointer"
+        @click="goToBack"
+      >
+        Back
+      </button>
     </div>
     <p v-else>Loading...</p>
   </div>
@@ -27,4 +33,7 @@ onMounted(() => {
   }
 });
 
+const goToBack = () => {
+  navigateTo("/");
+};
 </script>
